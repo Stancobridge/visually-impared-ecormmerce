5eb7eb68007c3c16f9a8e80da65cf788f1d313a0
stancobridge <stancoproof@gmail.com>
2023-12-05 19:03:32 +0100

    fix logo

--- components/Layouts/NavbarLayout.tsx
+++ components/Layouts/NavbarLayout.tsx
@@ -9,40 +9,38 @@ import { AcmeLogo, SearchIcon } from "../Icons";
 
 export const NavbarLayout = () => {
   return (
-    <>
-      <Navbar isBordered>
-        <NavbarContent justify="start">
-          <NavbarBrand className="mr-4">
-            <Link href="/">
-              <AcmeLogo />
-            </Link>
-            <Link href="/">
-              <p className="hidden sm:block font-bold text-inherit">
-                PairSales
-              </p>
-            </Link>
-          </NavbarBrand>
-        </NavbarContent>
+    <Navbar isBordered>
+      <NavbarContent justify="start">
+        <NavbarBrand className="mr-4">
+          <Link href="/">
+            <AcmeLogo />
+          </Link>
+          <Link href="/">
+            <p className="hidden sm:block font-bold text-inherit">
+              LoligriVisual
+            </p>
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
 
-        <NavbarContent as="div" className="items-center" justify="end">
-          <form>
-            <Input
-              classNames={{
-                base: "max-w-full sm:max-w-[20rem] h-10",
-                mainWrapper: "h-full",
-                input: "text-small",
-                inputWrapper:
-                  "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-              }}
-              placeholder="Type to search product... "
-              size="sm"
-              startContent={<SearchIcon size={18} />}
-              type="search"
-              name="search"
-            />
-          </form>
-        </NavbarContent>
-      </Navbar>
-    </>
+      <NavbarContent as="div" className="items-center" justify="end">
+        <form>
+          <Input
+            classNames={{
+              base: "max-w-full sm:max-w-[20rem] h-10",
+              mainWrapper: "h-full",
+              input: "text-small",
+              inputWrapper:
+                "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+            }}
+            placeholder="Type to search product... "
+            size="sm"
+            startContent={<SearchIcon size={18} />}
+            type="search"
+            name="search"
+          />
+        </form>
+      </NavbarContent>
+    </Navbar>
   );
 };
